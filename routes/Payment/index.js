453ef2b024430a29921fa97e994fcaefcb7b0387
abiral <abiral@innovatetech.co>
2020-08-10 16:19:05 +0545

    update webhook

--- routes/Payment/index.js
+++ routes/Payment/index.js
@@ -30,14 +30,20 @@ router.post("/create-payment-intent", requireLogin, async (req, res) => {
 router.post("/webhooks", async (req, res) => {
   try {
     const sig = req.headers["stripe-signature"];
-    const event = stripe.webhooks.constructEvent(
-      req.rawBody,
-      sig,
-      STRIPE_ENDPOINT_SECRET
-    );
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        sig,
+        STRIPE_ENDPOINT_SECRET
+      );
+    } catch (err) {
+      res.status(400).json({ message: `Webhook Error: ${err.message}` });
+    }
 
     switch (event.type) {
-      case "payment_intent.succeeded": {
+      case "payment_intent.succeeded":
         const { receipt_email, amount, created } = event.data.object;
 
         const emailAddress = receipt_email;
@@ -62,20 +68,13 @@ router.post("/webhooks", async (req, res) => {
             await newCreditTransaction.save();
           }
         }
-
-        res.json({
-          message: "success",
-          success: true,
-          receivedAmount,
-          emailAddress,
-          newCreditTransaction
-        });
         break;
-      }
 
       default:
-        return res.status(404).json({ message: "Event not found" });
+        return response.status(400).end();
     }
+
+    res.json({ received: true });
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
