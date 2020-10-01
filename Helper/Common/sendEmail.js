d851e056aaac6f00382df74200cbdcff593e42a2
abiral <abiral@innovatetech.co>
2020-10-01 12:46:11 +0545

    use zoho for sending email

--- Helper/Common/sendEmail.js
+++ Helper/Common/sendEmail.js
@@ -4,27 +4,37 @@ const { NODEMAILER_PASS } = process.env;
 
 module.exports = async ({ subject, receiver, htmlContent }) => {
   try {
+    // const transport = nodemailer.createTransport({
+    //   name: "smtp.gmail.com",
+    //   host: "smtp.gmail.com",
+    //   service: "gmail",
+    //   port: 465,
+    //   secure: true,
+    //   auth: {
+    //     user: "abiralsubedi119@gmail.com",
+    //     pass: NODEMAILER_PASS
+    //   }
+    // });
     const transport = nodemailer.createTransport({
-      name: "smtp.gmail.com",
-      host: "smtp.gmail.com",
-      service: "gmail",
+      name: "smtp.zoho.com",
+      host: "smtp.zoho.com",
       port: 465,
       secure: true,
       auth: {
-        user: "abiralsubedi119@gmail.com",
+        user: "abiral0999@gmail.com",
         pass: NODEMAILER_PASS
       }
     });
 
-    const message = {
-      from: "abiralsubedi119@gmail.com",
-      replyTo: "abiralsubedi119@gmail.com",
+    const email = {
+      from: "abiral0999@zohomail.com",
+      replyTo: "abiral0999@zohomail.com",
       to: receiver,
       subject: subject,
       html: htmlContent
     };
 
-    transport.sendMail(message, function(err, info) {
+    transport.sendMail(email, function(err, info) {
       if (err) {
         return "error";
       } else {
