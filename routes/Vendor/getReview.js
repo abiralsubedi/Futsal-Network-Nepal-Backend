59dc2caef77cb17113f2be2a9ffc5cc24ae2a244
abiral <abiral@innovatetech.co>
2020-10-09 13:57:08 +0545

    add get review pagination

--- routes/Vendor/getReview.js
+++ routes/Vendor/getReview.js
@@ -6,11 +6,16 @@ module.exports = async (req, res) => {
     const { vendorId } = req.params;
     const { _id: userId } = req.user;
 
+    const pageSize = +req.query.pageSize;
+    const currentPage = +req.query.currentPage;
+
     const review = await Review.find(
       { vendor: vendorId, user: { $ne: ObjectId(userId) } },
       { __v: 0 }
     )
       .sort({ reviewDate: -1 })
+      .skip(pageSize * currentPage - pageSize)
+      .limit(pageSize)
       .populate({ path: "user", select: "-hash -salt" });
 
     res.json(review);
